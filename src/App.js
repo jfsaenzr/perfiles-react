5b57eb63f1381013d33582ef1da00c0fa662c8a2
jfsaenzr <47581366+jfsaenzr@users.noreply.github.com>
2022-05-25 17:27:10 -0500

    *

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ function App() {
       <div className="contenedor-principal">
         <h1>Perfiles profesionales</h1>
         <Testimonio
-          nombre="Johan Saenz"
-          pais="Colombia"
-          imagen="ing1"
-          cargo="Ingeniero de software"
-          empresa="HeOn"
-          testimonio="La Ingeniería de Software es una de las ramas de las ciencias de la computación que estudia 
-          la creación de software confiable y de calidad, basándose en métodos y técnicas de ingeniería, y brindando soporte operacional y de mantenimiento."
-        />
-        <Testimonio
           nombre="Alejandra Sesquile"
           pais="Colombia"
           imagen="psic2"
@@ -24,6 +15,16 @@ function App() {
           testimonio="Ciencia que estudia los procesos mentales, las sensaciones, las percepciones y el comportamiento del ser humano, 
           en relación con el medio ambiente físico y social que lo rodea.."
         />
+        <Testimonio
+          nombre="Johan Saenz"
+          pais="Colombia"
+          imagen="ing1"
+          cargo="Ingeniero de software"
+          empresa="HeOn"
+          testimonio="La Ingeniería de Software es una de las ramas de las ciencias de la computación que estudia 
+          la creación de software confiable y de calidad, basándose en métodos y técnicas de ingeniería, y brindando soporte operacional y de mantenimiento."
+        />
+
       </div>
     </div>
   );
