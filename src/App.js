bf02e027e323b28c58a0461332b58900ffd8dd79
jfsaenzr <47581366+jfsaenzr@users.noreply.github.com>
2022-05-26 10:07:11 -0500

    *

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import './App.css';
-import { Testimonio } from './componentes/Testimonio';
+import { Perfil } from './componentes/Perfil';
+import data from "./data/Data.json"
 
 function App() {
+
   return (
     <div className="App">
       <div className="contenedor-principal">
         <h1>Perfiles profesionales</h1>
-        <Testimonio
-          nombre="Alejandra Sesquile"
-          pais="Colombia"
-          imagen="psic2"
-          cargo="Psicologa"
-          empresa="Clinica country"
-          testimonio="Ciencia que estudia los procesos mentales, las sensaciones, las percepciones y el comportamiento del ser humano, 
-          en relación con el medio ambiente físico y social que lo rodea.."
-        />
-        <Testimonio
-          nombre="Johan Saenz"
-          pais="Colombia"
-          imagen="ing1"
-          cargo="Ingeniero de software"
-          empresa="HeOn"
-          testimonio="La Ingeniería de Software es una de las ramas de las ciencias de la computación que estudia 
-          la creación de software confiable y de calidad, basándose en métodos y técnicas de ingeniería, y brindando soporte operacional y de mantenimiento."
-        />
+
+        {data.map((data, idx) => (
+          <Perfil key={idx}
+            nombre={data.nombre}
+            pais={data.pais}
+            imagen={data.imagen}
+            cargo={data.cargo}
+            empresa={data.empresa}
+            perfil={data.perfil}
+          />
+        ))}
 
       </div>
     </div>
---
+++ src/componentes/Perfil.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../css/Perfil.css';
+
+export function Perfil(props) {
+  return (
+    <div className="contenedor-perfil">
+      <img className="imagen-perfil"
+        src={require(`../imagenes/${props.imagen}.png`)}
+        alt="ing1" />
+      <div className="contenedor-texto-perfil">
+        <p className="nombre-perfil"><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className="cargo-perfil"><strong>{props.cargo}</strong> en {props.empresa}</p>
+        <p className="texto-perfil">"{props.perfil}"</p>
+      </div>
+    </div>
+  );
+};
+
+export default Perfil;
